120d98fb7e24676d628ef15e08dcf954de269793
zhangzju <zhangway19921221@163.com>
2016-08-30 02:38:05 -0700

    Initial commit

---
+++ config.py
@@ -0,0 +1,11 @@
+class Config(object):
+    pass
+
+class ProdConfig(object):
+    pass
+
+class DevConfig(object):
+    debug = True
+    # SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:19921221@127.0.0.1:3306/flask"
+    SQLALCHEMY_ECHO = True
